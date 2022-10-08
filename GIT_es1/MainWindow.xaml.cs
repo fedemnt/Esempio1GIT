7b7fbec8ce7195869d308b3990a94a5254f6c850
fedemnt <115340142+fedemnt@users.noreply.github.com>
2022-10-08 09:40:58 +0200

    Versione 2: aggiunta commento ciclo numeri random

--- GIT_es1/MainWindow.xaml.cs
+++ GIT_es1/MainWindow.xaml.cs
@@ -30,7 +30,7 @@ namespace GIT_es1
             double[] v = new double[10];
 
             Random rnd = new Random();
-
+            //Ciclo per caricare il vettore di numeri casuali
             for(int i = 0; i < 10; i++)
             {
                 v[i] = rnd.Next(0, 100);
